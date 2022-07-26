c4c65cefe2684747c583bbf06d7253dd8d3d0db7
Alpacabet <49300674+EricBuschUTD@users.noreply.github.com>
2022-07-25 22:52:11 -0500

    fixed ml predictor

--- python/server.py
+++ python/server.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask import request
 import json
 import pickle
+import numpy as np
 filename = 'ecg_svm'
 model = pickle.load(open(filename, 'rb'))
 # Flask constructor takes the name of
@@ -19,10 +20,27 @@ app = Flask(__name__)
 @app.route('/',methods = ['POST'])
 # ‘/’ URL is bound with hello_world() function.
 def ml_predict():
-    print(request.data)
+
+    
     try:
-        data = json.loads(request.data)['data']
-        model.predict(data)
+        ret =json.loads(request.get_json())['data1']
+        data = np.array(ret)
+     
+        prediction = model.predict(data)
+        abnormal = 0
+        normal = 0
+        
+        for x in prediction:
+            if x == 1:
+                normal +=1
+            else:
+                abnormal += 1
+        
+        result = {'normal': normal,
+                  'abnormal': abnormal}
+        
+        return result
+    
     except:
         return "Error parsing"
     return 'Hello World'
--- python/test.py
+++ python/test.py
@@ -1,10 +1,25 @@
 
 import requests
-
+import scipy.io
+import numpy as np
+import json
+from json import JSONEncoder
 ENDPOINT = "http://127.0.0.1:5000"
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
+x_test = scipy.io.loadmat('xtest.mat')['x_test'].astype(int)
+data0 = np.transpose(x_test)[0].tolist()
+data1 = np.transpose(x_test)[1].tolist()
 
 
-resp = requests.post(ENDPOINT, data="hello")
+json_str = json.dumps({'data1' :x_test.tolist()})
+print(json_str)
+
+resp = requests.post(ENDPOINT, json=json_str)
 
 if resp.status_code == 200:
     print(resp.content)
